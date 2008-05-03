fd8c876d44c18a39adf50083fc56264bf4d106cb
Miha Filej <miha.filej@gmail.com>
2008-05-03 02:37:00 +0200

    Added mark all read functionality
    On top of recent tweets a link is added that marks all messages as read by
    fading them and storing the most recent tweet in the url as an achor.
    
    If there are new tweets when the page is reloaded the count will be reflected
    on the badge of the dock icon.
    
    The mark all read link can be accessed with ctrl+a.

--- fluid_twitter.user.js
+++ fluid_twitter.user.js
@@ -8,9 +8,19 @@
 // @homepage        http://brionesandco.com/ryabriones
 // ==/UserScript==
 
+Element.addMethods({
+	markRead: function(element) {
+		element.setStyle({
+			opacity: '.75',
+			background: '#eee'
+		})
+	}
+});
+
 var FluidTwitter = Class.create({
 	initialize: function() {
 		this.trimUI();
+		this.allRead();
 		
 		setTimeout(this.refreshTwitter.bind(this), 30000);
 	},
@@ -34,6 +44,34 @@ var FluidTwitter = Class.create({
 	  
 		$('header').down('img').setStyle({paddingLeft: '10px'});
 	},
+
+	allRead: function() {
+		var history = $$('#timeline tr[id^=status]');
+		var last_read = $('status_'+window.location.href.split('#l').last());
+		var newest = history.first().id.sub(/status_/,'');
+		var unread = last_read.previousSiblings();
+
+		// when anchor not present
+		if (!last_read) last_read = history.last();
+	
+		last_read.nextSiblings().concat(last_read).invoke('markRead');
+		
+		if (unread.size() > 0) {
+			var link = new Element('a', { href: '#l'+newest, accesskey: 'a' }).update('mark <u>a</u>ll read');
+			link.observe('click', function() {
+				history.invoke('markRead')
+				fluid.setDockBadge("");
+				this.hide();
+			});
+			link.setStyle({
+					border: '1px solid #ccc', display: 'block', float: 'right',
+					'font-size': '10px', 'margin-top': '-21px', padding: '2px 6px'
+			});
+			$('timeline').insert({ before: link } );
+	
+			fluid.setDockBadge((unread.size() || "").toString());
+		}
+	},	
 	
 	refreshTwitter: function() {
 	  // don't refresh if i'm scrolling
